12799b54282318de16557819673fdfb24674a413
TaehoLEEKR <679748@naver.com>
2023-05-08 18:03:30 +0900

    Initial commit

---
+++ src/main/java/com/example/fintechproj/FintechProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.fintechproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fintechproj/FintechProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fintechproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
